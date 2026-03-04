fb8aa7a2fdb160286d9cc2805b0c71c6d9e7327a
BriolVictoria <victoria.briol@student.hepl.be>
2026-03-04 11:35:13 +0100

    feat: migrate to a simple view system

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -6,5 +6,5 @@ function index()
     $students = all();
     $title = 'Prendre les présences';
 
-    include VIEWS_PATH . '/attendances/index.php';
+    view('attendances.index', compact('title', 'students'));
 }
